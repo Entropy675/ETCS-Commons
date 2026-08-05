7e80a3eebb47054aa0359f1617c6319c49a5ac63
Sibte Kazmi <62915307+Entropy675@users.noreply.github.com>
2026-08-05 19:49:24 -0400

    Define CHESSPROVIDER_CONTRACT__
    Added preprocessor definition for CHESSPROVIDER_CONTRACT__ and removed platform-specific checks.

---
+++ ChessProvider/Contract_ChessProvider.h
@@ -0,0 +1,21 @@
+#ifndef CHESSPROVIDER_CONTRACT__
+// #if defined(__EMSCRIPTEN__)
+    // #define CHESSPROVIDER_CONTRACT__
+// #elif defined(_WIN32)
+    // #define CHESSPROVIDER_CONTRACT__
+// #elif defined(__linux__)
+    // #define CHESSPROVIDER_CONTRACT__
+// #else
+    // #warning "ChessProvider_Contract: Platform not detected. Check preprocessor definitions."
+    // #error "Unsupported platform"
+// #endif
+
+// because this code is OS invariant, we don't need to do the whole typedef shenanigans to have a concrete type...
+#include "ChessProvider/ChessBoard.h"
+#define CHESSPROVIDER_CONTRACT__
+
+// auto generated hashes of headers:
+#include "../../ETCS.h"
+#include "module_hashes.h"
+
+#endif // CHESSPROVIDER_CONTRACT__
